cd87eb1acd79b49429fd538af3a057eec29824bb
asif2508 <rakibul4210@gmail.com>
2022-10-02 19:00:28 +0600

    chart updated

--- src/views/dashboard/Dashboard.js
+++ src/views/dashboard/Dashboard.js
@@ -90,6 +90,7 @@ const Dashboard = () => {
                       }
                     }
                     ;
+                    chart.update("quiet")
                   }
                 }
               },
